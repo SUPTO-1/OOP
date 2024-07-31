4464a584ec7d240e6d31be6943fa945b11565102
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-07-31 13:39:28 +0600

    staticMemberFunctionexpamle

--- staticMemebrfunctionTwo.cpp
+++ staticMemebrfunctionTwo.cpp
@@ -1,2 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
+class students{
+public:
+  string name , id;
+  void input();
+  void display();
+  static int cnt;
+  static void totalStudent()
+  {
+    cout<<"Total Student: "<<cnt<<endl;
+  }
+};
+void students :: input()
+{
+  cout<<"Enter Your Name: ";
+  cin>>name;
+  cout<<"Enter Your Id: ";
+  cin>>id;
+  cnt++;
+}
+void students :: display()
+{
+  cout<<"Your Name: "<<name<<endl;
+  cout<<"Your Id: "<<id<<endl;
+}
+int students :: cnt;
+int main()
+{
+  students nine[20];
+  for(int i=1;i<=3;i++)
+  {
+    nine[0].input();
+    nine[0].display();
+    students :: totalStudent();
+  }
+}
